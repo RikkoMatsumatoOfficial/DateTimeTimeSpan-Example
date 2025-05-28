b547104ca775be0fb9600493d960ecd2211ef2a1
Rikko Matsumato <kuttimsx@hotmail.com>
2025-05-28 15:16:27 +0300

    Add files via upload

---
+++ TimeSpanDateTime-Example/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeSpanDateTime_Example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string date = DateTime.Now.ToString("h:mm tt");
+            timespantext.Text = date;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(343);
+        }
+    }
+}
